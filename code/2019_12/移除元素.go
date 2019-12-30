a2836049d5968af370e76747757cd508c916c3bf
tanzhihao <tanzhihao@cyou-inc.com>
2019-12-30 22:24:56 +0800

    移除元素

---
+++ code/2019_12/移除元素.go
@@ -0,0 +1,33 @@
+/**
+* @Author: Chicken dishes
+* @Date: 2019/12/30 22:23
+ */
+package main
+
+//给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+//
+//不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+//
+//元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+func removeElement(nums []int, val int) int {
+	i := 0
+	j := 0
+	for _, k := range nums {
+		if k != val {
+			nums[i] = k
+			i++
+			j++
+		} else {
+			j++
+		}
+	}
+	res := len(nums) - (j - i)
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3, 2, 4, 2}
+	val := 2
+	removeElement(nums, val)
+}
