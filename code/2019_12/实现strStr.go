34a124eecc1b763232559e6bca1e11ed5d6b8039
tanzhihao <tanzhihao@cyou-inc.com>
2019-12-31 21:27:45 +0800

    暴力解法

---
+++ code/2019_12/实现strStr.go
@@ -0,0 +1,31 @@
+/**
+* @Author: Chicken dishes
+* @Date: 2019/12/31 21:02
+ */
+package main
+
+import "fmt"
+
+//给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	lenN := len(needle)
+	lenH := len(haystack)
+	for i := 0; lenH-lenN >= i; i++ {
+		if haystack[i:i+lenN] == needle {
+			fmt.Println(i)
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	haystack := "aaaaa"
+	needle := "bba"
+
+	strStr(haystack, needle)
+}
